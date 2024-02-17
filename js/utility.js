b51d26aede646eea2815909a784122047bbee7c8
AmdadulHQ <rimonamdadul301@gmail.com>
2024-02-18 01:47:04 +0600

    Almost Done Seat count and click seat js

---
+++ js/utility.js
@@ -0,0 +1,45 @@
+const seats = document.querySelectorAll(".seat");
+
+function setElementById(elementId, value) {
+  const element = document.getElementById(elementId);
+  const li = document.createElement("li");
+  li.setAttribute("class", "font-inter");
+  li.innerText = value;
+  element.appendChild(li);
+}
+function setBackgroundColor() {}
+
+function getElementById(elementId) {
+  const element = document.getElementById(elementId);
+  elementInnerText = parseInt(element.innerText);
+  return elementInnerText;
+}
+
+const array = [];
+
+let totalSeat = 40;
+
+
+for (let seat of seats) {
+  seat.addEventListener("click", function () {
+    const price = 550;
+    const seatName = seat.innerText;
+
+
+    if (array.includes(seatName) === false) {
+      array.push(seatName);
+        totalSeat = totalSeat - 1;
+        
+        if(array.length > 4){
+            alert('You Buy Maximum Ticket')
+            totalSeat = totalSeat - 1;
+            return;
+        }
+    }
+    document.getElementById('seat-left').innerText = totalSeat
+    setElementById("seat-name", seatName);
+    setElementById("class", "Economoy");
+    setElementById("seat-price", price);
+    seat.classList.add("bg-[rgb(29,209,0)]","transition-[500ms]");
+  });
+}
